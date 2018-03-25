b0e0b2d3cef3fd1ebf980ca829b195ab9e5331e4
xsa <saleksey67@gmail.com>
2018-03-25 08:33:56 +0300

    97

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -8,16 +8,13 @@ namespace LanguageFeatures.Controllers
     {
         public ViewResult Index()
         {
-            List<string> results = new List<string>();
-
-            foreach (Product p in Product.GetProducts())
+            Dictionary<string, Product> products = new Dictionary<string, Product>
             {
-                string name = p?.Name ?? "<No Name>";
-                decimal? price = p?.Price ?? 0;
-                string relatedName = p?.Related?.Name ?? "<None>";
-                results.Add($"Name: {name}, Price: {price}, Related: {relatedName}");
-            }
-            return View(results);
+                ["Kayak"] = new Product { Name = "Kayak", Price = 275M },
+                ["Lifejacket"] = new Product { Name = "Lifejacket", Price = 48.95M }
+            };
+        
+            return View("Index", products.Keys);
         }
     }
 }

dcc8380e41bd0ec6cc6ed893d3e4f673260671b8
xsa <saleksey67@gmail.com>
2018-03-25 08:33:39 +0300

    97

---
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using LanguageFeatures.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController: Controller
+    {
+        public ViewResult Index()
+        {
+            List<string> results = new List<string>();
+
+            foreach (Product p in Product.GetProducts())
+            {
+                string name = p?.Name ?? "<No Name>";
+                decimal? price = p?.Price ?? 0;
+                string relatedName = p?.Related?.Name ?? "<None>";
+                results.Add($"Name: {name}, Price: {price}, Related: {relatedName}");
+            }
+            return View(results);
+        }
+    }
+}
--- LanguageFeatures/Startup.cs
+++ LanguageFeatures/Startup.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
@@ -16,11 +12,14 @@ namespace LanguageFeatures
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
+            app.UseMvcWithDefaultRoute();
+
             loggerFactory.AddConsole();
 
             if (env.IsDevelopment())

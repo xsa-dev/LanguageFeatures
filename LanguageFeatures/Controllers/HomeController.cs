97555d05bc57268e99baf34845a11d85262f5391
xsa <saleksey67@gmail.com>
2018-03-28 21:03:02 +0300

    105

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace LanguageFeatures.Controllers
             ShoppingCart cart = new ShoppingCart { Products = Product.GetProducts() };
 
             Product[] productArray = {
-                new Product { Name = "Kayak", Price = 275M },
+                new Product {Name = "Kayak", Price = 275M },
                 new Product {Name = "Lifejacket", Price = 48.95M },
                 new Product {Name = "Soccer ball", Price = 19.50M },
                 new Product {Name = "Corner flag", Price = 34.95M }
@@ -28,7 +28,7 @@ namespace LanguageFeatures.Controllers
             };
 
             decimal priceFilterTotal = productArray
-                .Filter(FilterByPrice)
+                .Filter(p => (p?.Price ?? 0) >= 20)                
                 .TotalPrices();
 
             decimal nameFilterTotal = productArray

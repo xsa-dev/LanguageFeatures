fa6ddfba8d64962ab4b3a592bb83f2ddb0d70f18
xsa <saleksey67@gmail.com>
2018-03-28 22:23:22 +0300

    114

--- LanguageFeatures/Controllers/HomeController.cs
+++ LanguageFeatures/Controllers/HomeController.cs
@@ -7,10 +7,17 @@ namespace LanguageFeatures.Controllers
 {
     public class HomeController : Controller
     {
-        public async Task<ViewResult> Index()
+        public ViewResult Index()
         {
-            long? length = await MyAsyncMethods.GetPageLength();
-            return View(new string[] { $"Length: {length}" });
+
+            var products = new[]
+            {
+                new { Name = "Kayak", Price = 275M },
+                new { Name = "Lifejacket", Price = 48.95M },
+                new { Name = "Soccer ball", Price = 19.50M },
+                new { Name = "Corner flag", Price = 34.95M }
+            };
+            return View(products.Select(p => $"Name: {p.Name}, Price: {p.Price}"));
         }
     }
 }
